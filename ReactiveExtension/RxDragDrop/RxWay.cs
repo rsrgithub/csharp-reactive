a77be74b94a76c455dd0ceabb2adf3453c9199d5
Rajendra Singh <rajendra.singh@machiningcloud.com>
2018-11-05 16:03:09 -0800

    Renamed the project examples

--- ReactiveExtension/RxDragDrop/RxWay.cs
+++ ReactiveExtension/RxDragDrop/RxWay.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace ReactiveExtension
+namespace RxDragDrop
 {
     public class RxWay
     {
