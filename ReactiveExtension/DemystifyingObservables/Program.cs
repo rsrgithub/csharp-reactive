eb1c0c6ce6278e52d3452f8d5c0e66893252d5f7
Rajendra Singh <rajendrasinghrajput@gmail.com>
2018-11-08 20:42:58 -0800

    Select method

--- ReactiveExtension/DemystifyingObservables/Program.cs
+++ ReactiveExtension/DemystifyingObservables/Program.cs
@@ -11,11 +11,12 @@ namespace DemystifyingObservables
     {
         static void Main(string[] args)
         {
-            var myObservable = new Observable((observer) =>
+            var num = 0;
+            var myObservable = new Observable<int>((observer) =>
             {
-                var dataSource = new DataSource();                
-                var safeObs = new SafeObserver(observer);
-                dataSource.OnData = o =>  safeObs.Next(o);
+                var dataSource = new DataSource();
+                var safeObs = new SafeObserver<int>(observer);
+                dataSource.OnData = o => safeObs.Next(o);
                 dataSource.OnError = (err) => safeObs.Error(err);
                 dataSource.OnComplete = () => safeObs.Complete();
                 safeObs.Unsub = () => dataSource.Destroy();
@@ -24,41 +25,37 @@ namespace DemystifyingObservables
 
 
 
-            myObservable.Subscribe(new MyObserver());
-
+            var subscription = myObservable.Select(x => x * 2).Subscribe(new MyObserver<int>
+            {
+                Next = Console.WriteLine,
+                Error = Console.WriteLine,
+                Complete = () => Console.WriteLine("Complete")
+            });            
+            
             Console.ReadKey();
-        }        
+        }
     }
 
-    public class MyObserver : IObserver
+    public class MyObserver<T> : IObserver<T>
     {
-        public void Next(object value)
-        {
-            Console.WriteLine(value);
-        }
+        public Action<T> Next { get; set; }
 
-        public void Error(object error)
-        {
-            Console.WriteLine(error);
-        }
+        public Action<T> Error { get; set; }
 
-        public void Complete()
-        {
-            Console.WriteLine("Complete!");
-        }
+        public Action Complete { get; set; }
     }
 
     public class DataSource
     {
         private readonly Timer _timer;
-        public Action<object> OnData;
-        public Action<object> OnError;
+        public Action<int> OnData;
+        public Action<int> OnError;
         public Action OnComplete;
 
         public DataSource()
         {
             int i = 0;
-            _timer = new Timer(state => Emit(i++), null, TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(1));            
+            _timer = new Timer(state => Emit(i++), null, TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(1));
         }
 
         private void Emit(int i)
@@ -71,7 +68,7 @@ namespace DemystifyingObservables
             {
                 OnComplete?.Invoke();
                 Destroy();
-            }            
+            }
         }
 
         public void Destroy()
@@ -80,21 +77,23 @@ namespace DemystifyingObservables
         }
     }
 
-    public interface IObserver
+    public interface IObserver<T>
     {
-        void Next(object value);
-        void Error(object error);
-        void Complete();
+        Action<T> Next { get; set; }
+
+        Action<T> Error { get; set; }
+
+        Action Complete { get; set; }
     }
 
-    public class SafeObserver
+    public class SafeObserver<T>
     {
         private bool _isUnsubscribed;
         public Action Unsub;
-        private readonly IObserver _target;
+        private readonly IObserver<T> _target;
         public Action Unsubscribe;
 
-        public SafeObserver(IObserver target)
+        public SafeObserver(IObserver<T> target)
         {
             _target = target;
             Unsubscribe = () =>
@@ -105,7 +104,7 @@ namespace DemystifyingObservables
             };
         }
 
-        public void Next(object value)
+        public void Next(T value)
         {
             if (!_isUnsubscribed)
             {
@@ -118,10 +117,10 @@ namespace DemystifyingObservables
                     Unsubscribe();
                     throw;
                 }
-            } 
+            }
         }
 
-        public void Error(object err)
+        public void Error(T err)
         {
             if (!_isUnsubscribed)
             {
@@ -155,20 +154,36 @@ namespace DemystifyingObservables
             }
 
             Unsubscribe();
-        }                
+        }
     }
 
-    public class Observable
+    public class Observable<T>
     {
-        private Func<IObserver, object> _observer;
-        public Observable(Func<IObserver, object> observer)
+        private Func<IObserver<T>, Action> _observer;
+        public Observable(Func<IObserver<T>, Action> observer)
         {
             _observer = observer;
         }
 
-        public object Subscribe(IObserver observer)
+        public Action Subscribe(IObserver<T> observer)
         {
             return _observer(observer);
+        }
+
+
+        public Observable<T> Select(Func<T, T> projectFunc)
+        {
+            return new Observable<T>((observer) =>
+            {
+                IObserver<T> newObserver = new MyObserver<T>
+                {
+                    Next = (v) => observer.Next(projectFunc(v)),
+                    Error = (e) => observer.Error(e),
+                    Complete = () => observer.Complete()
+                };
+
+                return Subscribe(newObserver);
+            });
         }
     }
 }
